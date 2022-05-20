617bfd681dbbd41e928d42ba09545c441295a6e9
Shane Dorsey <scdorsey22@gmail.com>
2022-05-19 17:17:24 -0700

    commit tree da8bfdef26b730ccbec3b05ab707674d87572e56

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+const number = []
+
+function countDown(number) {
+  let i =10
+  while (i >= 0) {
+   console.log(i--); 
+  }
+  return number
+}
+console.log(countDown())
